84277f24f2496c700165a265f96fcdd332a78833
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2025-05-28 11:18:41 +0530

    Moved Sorting to Custom

--- Data_Structure/linklist.cpp
+++ Data_Structure/linklist.cpp
@@ -1,28 +1,22 @@
 #include "link_list.hpp"
 #include<string>
-
+#include<vector>
 int main(int argc, char const *argv[])
 {
-    SingleList<char *> dummy;
-    dummy.insert("Hello");
-    dummy.insert("World");
-    dummy.insert("From", 1);
-    dummy.insert("C++", 0);
-    dummy.insert("C++", 4);
-    dummy.insert("C++", 5);
-    SingleList<int> dummy2 ;
-    dummy2.insert(10);
-    dummy2.insert(-14);
-    dummy2.insert(28);
-
-    dummy.print();
-    dummy2.print();
-    printf("%d\n",dummy2.size);
-    SingleList<std::string> dummy3;
-    dummy.insert("A");
-    dummy3.insert("B");
-    dummy.print();  // shows both A and B
-    dummy3.print(); // shows the same A and B
+    SingleList<std::string> *dummy = new SingleList<std::string>;
+    dummy->data = "10";
+    printf("%s\n", dummy->data);
+    dummy->insert("Hello");
+    dummy->insert("World");
+    dummy->insert("From", 1);
+    dummy->insert("C++", 0);
+    dummy->insert("C++", 4);
+    dummy->insert("C++", 5);
+    dummy->print();
+    dummy->remove(3);
+    dummy->print();
+    printf("%lld\n", dummy->size());
     
+    // delete[] dummy;
     return 0;
 }
