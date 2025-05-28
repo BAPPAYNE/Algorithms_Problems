bd632a88eab915ea2fc4483cb3f22b8651e27641
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2025-05-28 08:47:56 +0530

    Refactor SingleList to use instance variable for size and implement remove method

--- Data_Structure/link_list.hpp
+++ Data_Structure/link_list.hpp
@@ -8,13 +8,18 @@
 
 template<typename type>
 struct SingleList {
+    private:
+        long long size_ = 0;
+
     public:
         type data;
         SingleList *next = nullptr;
         SingleList *head = nullptr;
-        long long size = 0;
         SingleList *tail = nullptr;
 
+        ~SingleList(){
+            clear();
+        }
         unsigned insert(type new_Value, int position = -1){
             // Invalid position
             if(position < -1){
@@ -22,7 +27,7 @@ struct SingleList {
                 return 1;
             }
             // A position where node cannot be inserted.
-            if(position > size && position != -1){
+            if(position > size_ && position != -1){
                 fprintf(stderr,"Insert position out of Range\n") ;
                 return 1;
             }
@@ -38,7 +43,8 @@ struct SingleList {
             // Special handling for pointer types
             if constexpr (std::is_pointer_v<type>) {
                 new_Node->data = new_Value;  // Simple assignment for pointers
-            } else {
+            }
+            else {
                 new (&new_Node->data) type(new_Value);  // Placement new for objects
             }
             // new (&new_Node->data) type(new_Value);
@@ -48,7 +54,7 @@ struct SingleList {
             if(head == nullptr && position == -1){
                 head = new_Node ;
                 tail = new_Node;
-                size = 1 ;
+                size_ = 1 ;
                 return 0;
             }
 
@@ -56,7 +62,7 @@ struct SingleList {
             if(position == -1){
                 tail->next = new_Node;
                 tail = new_Node;
-                size++;
+                size_++;
                 return 0;
             }
 
@@ -64,7 +70,7 @@ struct SingleList {
             if(position == 0){
                 new_Node->next = head ;
                 head = new_Node;
-                size++;
+                size_++;
                 return 0;
             }
             SingleList *current = head ;
@@ -79,23 +85,94 @@ struct SingleList {
             if (new_Node->next == nullptr) {
                 tail = new_Node;
             }
-            size++;
+            size_++;
             return 0 ;
+        }
+
+
+        // remove node function 
+        unsigned remove(long long position = -1){
+            // SingleList is empty
+            if(head == nullptr){
+                return 1;
+            }
+
+            if(position < -1){
+                fprintf(stderr,"Error: Position cannot be negative (except -1 for end)\n");
+                return 1;
+            }
+
+            if(position >= size_ && position != -1){
+                fprintf(stderr,"delete position out of Range");
+                return 1;
+            }
+            if(position == -1 || position == size_-1){
+                if(head == tail){
+                    tail->data.~type();
+                    free(tail);
+                    head = tail = nullptr;
+                    size_ = 0;
+                    return 0;
+                }
+                if(tail != nullptr){
+                    SingleList<type> *current = head;
+                    while (current->next->next != nullptr)
+                    {
+                        current = current->next;
+                    }
+                    tail = current ;
+                    current->data.~type();
+                    free(current->next);
+                    current->next = nullptr ;
+                    size_--;
+                    return 0;
+                }
+                return 1;
+            }
+            // Delete at beginning
+            if(position == 0){
+                SingleList *temp = head;
+                head = temp->next;
+                temp->data.~type();
+                free(temp);
+                size_--;
+                return 0;
+            }
+            long long current_Pos = 0;
+            SingleList<type> *current = head;
+            while(current->next!=nullptr && current_Pos + 1 != position){
+                current=current->next;
+                current_Pos++;
+            }
+            SingleList<type> *temp = current->next;
+            current->next = current->next->next;
+            temp->data.~type();
+            free(temp);
+            size_--;
+            return 0;
+            
         }
 
 
         // Printing SingleList
         void print() const {
+            // if(size_ == 0){
+            //     fprintf(stderr,"");
+            // }
             SingleList<type> *current = head;
             while (current != nullptr) {
                 std::cout << current->data << " -> ";
                 current = current->next;
             }
+            // for(long long i = 0 ; i < size_ ; i++){
+            //     std::cout << current->data << " -> ";
+            //     current = current->next;
+            // }
             std::cout << "nullptr\n";
         }
 
-        // Destroying SingleList
-        void destroy() {
+        // Clearing(Emptying) SingleList
+        void clear() {
             SingleList* current = head;
             while (current) {
                 current->data.~type(); // Call destructor
@@ -104,18 +181,39 @@ struct SingleList {
                 free(temp); // Free memory
             }
             head = nullptr;
-            size = 0;
+            size_ = 0;
         }
 
-    // Some private things
-    private:
-        SingleList *get_Node(long long position){
+        SingleList *get(long long position){
             SingleList *current = head;
             for(long long i = 0 ; i < position ; i++){
                 current = current->next;
             }
             return current ;
         }
+
+        SingleList *clone() const{
+            SingleList<type> new_List;
+                SingleList<type> *current = head;
+                while (current != nullptr) {
+                    new_List.insert(current->data);
+                    current = current->next;
+                }
+            return new_List;
+            
+        }
+
+        bool isempty() const{
+            return head == nullptr;
+        }
+
+
+        long long size() const{
+            return size_ ;
+        }
+        
+    // Some private things
+    private:
 
 };
 // Initialize static members
@@ -126,7 +224,7 @@ struct SingleList {
 // SingleList<type> *SingleList<type>::tail = nullptr;
 
 // template<typename type>
-// long long SingleList<type>::size = 0;
+// long long SingleList<type>::size_ = 0;
 
 // int main(int argc, char const *argv[])
 // {
