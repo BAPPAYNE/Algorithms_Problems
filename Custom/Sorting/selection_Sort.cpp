7b35174a1b762390544006efa1bff755750bae67
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2025-06-02 16:57:42 +0530

    Refactor selection sort implementation to use array instead of vector and clean up commented code

--- Custom/Sorting/selection_Sort.cpp
+++ Custom/Sorting/selection_Sort.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
-#include <vector>
+// #include <vector>
+#include<chrono>
 using namespace std;
 
 
-
- struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
- };
- 
-
 // Function to perform selection sort
-void selectionSort(vector<int>& arr) {
-    int n = arr.size();
-    for(int i = 0; i < n - 1; ++i) {
+// void selectionSort(vector<int>& arr) {
+//     int size = arr.size();
+//     for(int i = 0; i < size - 1; ++i) {
+//         int minIdx = i;
+//         for(int j = i + 1; j < size; ++j) {
+//             if(arr[j] < arr[minIdx]) {
+//                 minIdx = j;
+//             }
+//         }
+//         swap(arr[i], arr[minIdx]);
+//     }
+// }
+void selectionSort(int arr[],int size) {
+    for(int i = 0; i < size - 1; ++i) {
         int minIdx = i;
-        for(int j = i + 1; j < n; ++j) {
+        for(int j = i + 1; j < size; ++j) {
             if(arr[j] < arr[minIdx]) {
                 minIdx = j;
             }
@@ -28,22 +30,25 @@ void selectionSort(vector<int>& arr) {
 }
 
 int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter elements:\n";
-    for(int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
+    // int size;
+    // cout << "Enter sizeumber of elements: ";
+    // cin >> size;
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    // cout << "Enter elements:\n";
+    // for(int i = 0; i < size; ++i) {
+    //     cin >> arr[i];
+    // }
+    int size = sizeof(arr)/sizeof(arr[0]) ;
+    auto T1 = chrono::high_resolution_clock::now();
+    selectionSort(arr,size);
 
-    selectionSort(arr);
+    auto diff_ = chrono::high_resolution_clock::now() - T1 ;
+    cout << chrono::duration_cast<chrono::duration<double,milli>> (diff_).count() << " milliseconds" ;
 
-    cout << "Sorted array:\n";
-    for(int i = 0; i < n; ++i) {
+    // cout << "Sorted array:\n";
+    for(int i = 0; i < size; ++i) {
         cout << arr[i] << " ";
     }
-    cout << endl;
 
     return 0;
 }
